e55fb18c931a1dfc5c72e3037b3e4b32eae4ee28
vinrt <vny.raghav@gmail.com>
2019-08-02 02:49:28 +0200

    Changes to be committed: 	modified:   SpringbootIOT/src/main/java/com/spring/iot/SpringbootIOT/SpringbootIotApplication.java 	modified:   SpringbootIOT/src/main/resources/application.properties 	.DS_Store 	SpringbootIOT/src/main/java/com/spring/iot/SpringbootIOT/Controller/ 	SpringbootIOT/src/main/java/com/spring/iot/SpringbootIOT/Service/

--- SpringbootIOT/src/main/java/com/spring/iot/SpringbootIOT/SpringbootIotApplication.java
+++ SpringbootIOT/src/main/java/com/spring/iot/SpringbootIOT/SpringbootIotApplication.java
@@ -2,7 +2,10 @@ package com.spring.iot.SpringbootIOT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
+@ComponentScan("com.spring")
 @SpringBootApplication
 public class SpringbootIotApplication {
 
