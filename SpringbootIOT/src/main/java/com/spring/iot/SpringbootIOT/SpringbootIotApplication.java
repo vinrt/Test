dabc6e9a617eebb7954a140ecf259a40105101ec
vinrt <vny.raghav@gmail.com>
2019-08-02 02:13:25 +0200

    Initial commit

---
+++ SpringbootIOT/src/main/java/com/spring/iot/SpringbootIOT/SpringbootIotApplication.java
@@ -0,0 +1,14 @@
+package com.spring.iot.SpringbootIOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIotApplication.class, args);
+		System.out.println("Hi IOT");
+	}
+
+}
